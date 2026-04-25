2a0a21971426d309ebd324f2da15c1115b1aadd7
edermanoel94 <edercosta94@live.com>
2026-04-25 10:47:21 -0300

    feat: with better format

--- go.mod
+++ go.mod
@@ -1,11 +1,17 @@
 module github.com/edermanoel94/fmtstack
 
-go 1.24.0
+go 1.26
 
-require github.com/fatih/color v1.18.0
+require (
+	github.com/fatih/color v1.18.0
+	golang.design/x/clipboard v0.7.1
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/exp/shiny v0.0.0-20250606033433-dcc06ee1d476 // indirect
+	golang.org/x/image v0.28.0 // indirect
+	golang.org/x/mobile v0.0.0-20250606033058-a2a15c67f36f // indirect
+	golang.org/x/sys v0.33.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,43 +1,153 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
+	"fmt"
 	"log"
-	"os"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
+	"golang.design/x/clipboard"
 )
 
-const (
-	chunkSize = 2048
+type PayloadMessage struct {
+	Body         string            `json:"Body"`
+	SeverityText string            `json:"SeverityText"`
+	StackTrace   string            `json:"Stacktrace"`
+	Attributes   map[string]string `json:"Attributes"`
+	Timestamp    time.Time         `json:"time"`
+}
+
+var (
+	cyan       = color.New(color.FgCyan, color.Bold).SprintFunc()
+	yellow     = color.New(color.FgYellow).SprintFunc()
+	yellowBold = color.New(color.FgYellow, color.Bold).SprintFunc()
+	green      = color.New(color.FgGreen).SprintFunc()
+	gray       = color.New(color.FgHiBlack).SprintFunc()
+	whiteBold  = color.New(color.FgWhite, color.Bold).SprintFunc()
+	magenta    = color.New(color.FgMagenta).SprintFunc()
 )
 
-func main() {
-
-	bufReader := bufio.NewReader(os.Stdin)
-
-	buf := make([]byte, chunkSize)
-
-	_, err := bufReader.Read(buf)
-
-	if err != nil {
+func init() {
+	if err := clipboard.Init(); err != nil {
 		log.Fatal(err)
 	}
+}
 
-	data := string(buf)
+func main() {
+	data := clipboard.Read(clipboard.FmtText)
 
-	lines := strings.Split(data, "\\n")
+	var (
+		stackTraceData string
+		payloadMsg     PayloadMessage
+	)
 
-	for _, line := range lines {
+	if json.Valid(data) {
+		if err := json.Unmarshal(data, &payloadMsg); err != nil {
+			log.Fatal(err)
+		}
+		stackTraceData = payloadMsg.StackTrace
+	} else {
+		stackTraceData = string(data)
+	}
 
-		if len(line) == 0 {
+	var (
+		pendingFunc        string
+		pendingIsCreatedBy bool
+		needBlank          bool
+	)
+
+	emit := func(funcLine, fileLine string, isCreatedBy bool) {
+		if needBlank {
+			fmt.Println()
+		}
+		isUser := fileLine != "" &&
+			!strings.Contains(fileLine, "/usr/local/go/src/") &&
+			!strings.Contains(fileLine, "/go/pkg/mod/")
+
+		switch {
+		case isCreatedBy:
+			fmt.Println(magenta(funcLine))
+		case isUser:
+			fmt.Println(yellowBold(funcLine))
+		default:
+			fmt.Println(yellow(funcLine))
+		}
+		if fileLine != "" {
+			fmt.Println(formatFileLine(fileLine, isUser))
+		}
+		needBlank = true
+	}
+
+	flushPending := func() {
+		if pendingFunc != "" {
+			emit(pendingFunc, "", pendingIsCreatedBy)
+			pendingFunc = ""
+			pendingIsCreatedBy = false
+		}
+	}
+
+	stackTraceData = strings.ReplaceAll(stackTraceData, "\\n", "\n")
+
+	for line := range strings.SplitSeq(stackTraceData, "\n") {
+		line = strings.ReplaceAll(line, "\\t", "\t")
+		if strings.TrimSpace(line) == "" {
 			continue
 		}
 
-		cleaned := strings.Replace(line, "\\t", "	", 1)
+		switch {
+		case isGoroutineHeader(line):
+			flushPending()
+			if needBlank {
+				fmt.Println()
+			}
+			fmt.Println(cyan(line))
+			needBlank = false
 
-		color.Red(cleaned)
+		case strings.HasPrefix(line, "\t"):
+			if pendingFunc != "" {
+				emit(pendingFunc, line, pendingIsCreatedBy)
+				pendingFunc = ""
+				pendingIsCreatedBy = false
+			} else {
+				if needBlank {
+					fmt.Println()
+				}
+				fmt.Println(gray(line))
+				needBlank = true
+			}
 
+		default:
+			flushPending()
+			pendingFunc = line
+			pendingIsCreatedBy = strings.HasPrefix(line, "created by ")
+		}
 	}
+	flushPending()
+}
+
+func isGoroutineHeader(s string) bool {
+	return strings.HasPrefix(s, "goroutine ") && strings.HasSuffix(s, ":")
+}
+
+func formatFileLine(line string, isUser bool) string {
+	rest := strings.TrimPrefix(line, "\t")
+	var suffix string
+	if i := strings.LastIndex(rest, " "); i > 0 {
+		suffix = rest[i:]
+		rest = rest[:i]
+	}
+	var dir, fileColon string
+	if i := strings.LastIndex(rest, "/"); i >= 0 {
+		dir = rest[:i+1]
+		fileColon = rest[i+1:]
+	} else {
+		fileColon = rest
+	}
+	dirColored := gray(dir)
+	if isUser {
+		dirColored = green(dir)
+	}
+	return "\t" + dirColored + whiteBold(fileColon) + gray(suffix)
 }
